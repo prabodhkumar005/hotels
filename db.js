02d8664e6b59ee232aa1992fd90b17a98608c995
prabodhkumar005 <prabodhkumar7254@gmail.com>
2024-11-19 15:17:06 +0530

    shifted to online mongodb server

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose=require('mongoose');
-const mongoURL='mongodb://127.0.0.1:27017/hotel'
+require('dotenv').config();
+//const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL;
 mongoose.connect(mongoURL)
 
 const db=mongoose.connection;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db=require('./db')
-
+require('dotenv').config();
 const bodyParser=require('body-parser');
 app.use(bodyParser.json())
+
+const PORT=process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('welcome to my website')
@@ -15,6 +17,8 @@ app.use('/person',personRoutes)
 const menuItemRoutes=require('./routers/menuItemRoutes');
 app.use('/menuitem',menuItemRoutes)
 
-app.listen(3000,()=>{
+
+
+app.listen(PORT,()=>{
     console.log("listening on port 3000")
 })
